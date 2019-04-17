b45effa8138069cda1c281b294384ad2630e6f61
HuangRui <huangrui_@whu.edu.cn>
2019-04-17 14:13:13 +0800

    19.4.17 update

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const jsonServer = require('json-server');
 const express=require('express');
 const path=require('path');
-const  server=jsonServer.create();
+const server=jsonServer.create();
 const router=jsonServer.router('db.json');
 const middlewares=jsonServer.defaults();
+const port=process.env.LEANCLOUD_APP_PORT || 3000;
 const root=__dirname+'/build';
 server.use(express.static(root,{maxAge:86400000}));
 server.use(router);
@@ -11,7 +12,7 @@ server.use(middlewares);
 const reactRouterWhiteList=['/create','/edit/:itemid'];
 server.get(reactRouterWhiteList,(requst,response)=>{
 	response.sendFile(path.resolve(root,'index.html'));
-})
-server.listen(3000,()=>{
+});
+server.listen(port,()=>{
 	console.log('server is running');
 });
